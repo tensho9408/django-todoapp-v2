a801e29bf24d07b2aee28ae6723824783ddf2b53
tensho9408 <tenshoohashi@gmail.com>
2021-07-25 00:45:28 +0900

    ten

---
+++ todoproject/temp/settings/local.py
@@ -0,0 +1,9 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-d*)itqwi+=siveq@)0b%lb(!-*2z9z%b91)xbys0t3tjc8h@#z'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["django-todoapp-v2.herokuapp.com", "127.0.0.1"]
+
+
---
+++ todoproject/temp/settings/production.py
@@ -0,0 +1,9 @@
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["django-todoapp-v2.herokuapp.com", "127.0.0.1"]
